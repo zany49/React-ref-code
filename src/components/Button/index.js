5530b4fa9c397c765c2715d3c27da06963a932d4
zany49 <abdul.k@doodle-blue.com>
2024-09-20 17:34:29 +0530

    json server

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,109 +1,134 @@
-import React, { useEffect, useState } from "react";
-import "./style.css";
-import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from 'react';
+import './style.css' 
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Button = () => {
-  const [count, setCount] = useState(0);
-  const [dupcount, setDupCount] = useState(0);
-  const [itemList, setItemList] = useState([]);
-  const [name, setName] = useState("");
-  const [modelName, setModelName] = useState("");
-  const [imgLink, setImgLink] = useState("");
 
-  const navigate = useNavigate();
+const Button = () =>{
+    const [count, setCount] = useState(0)
+    const [dupcount, setDupCount] = useState(0)
+    const[itemList,setItemList] = useState([])
+    const[name,setName] = useState("")
+    const[modelName,setModelName] = useState("")
+    const[imgLink,setImgLink] = useState("")
 
-  const updateArr = (namemmm) => {
-    console.log("print--->", namemmm);
-    let arr = [
-      ...itemList,
-      {
-        name: name,
-        mobileModel: modelName,
-        img: imgLink,
-        id: itemList.length + 1,
-      },
-    ];
-    setItemList(arr);
-    setName("");
-    setModelName("");
-    setImgLink("");
-  };
+    const navigate = useNavigate()
 
-  const navigateMe = (data) => {
-    console.log("data---->", data);
-    navigate(`${data?.id}`, {
-      state: data,
-    });
-    // navigate(`/login`)
-  };
+    useEffect(()=>{
+        getProducts()
+    },[])
 
-  console.log(itemList);
+    const getProducts = async ()=>{
+        try{
 
-  return (
-    <>
-      {/* <button className='btn' onClick={dupaddNum}>Submit</button>
+            let res = await axios.get("http://localhost:5000/products")
+            console.log("res--->",res)
+            setItemList(res.data)
+
+        }catch(e){
+            console.log("e--->",e)
+        }
+
+    }
+    const updateArr = async (namemmm)=>{
+        try{
+            console.log("print--->",namemmm)
+            // let arr = [...itemList,{
+            //     name:name,
+            //     mobileModel:modelName,
+            //     img:imgLink,
+            //     id:itemList.length + 1
+            // }]
+            // setItemList(arr)
+            let res = await axios.post("http://localhost:5000/products",{
+                    name:name,
+                    mobileModel:modelName,
+                    img:imgLink,
+                    id:itemList.length + 1
+            })
+            console.log("res--->",res)
+            getProducts()
+            setName('')
+            setModelName('')
+            setImgLink('')
+        }catch(e){
+            console.log("e--->",e)
+        }
+
+    }
+
+    const navigateMe = (data)=>{
+        console.log("data---->",data)
+        navigate(`${data?.id}`,{
+            state: data
+        })
+        // navigate(`/login`)
+
+    }
+
+    console.log(itemList)
+
+    return(
+        <>
+        {/* <button className='btn' onClick={dupaddNum}>Submit</button>
         <p>cout - {count}</p>
         <p>dupcout - {dupcount}</p> */}
 
-      <input
-        type="text"
-        placeholder="Enter Mobile Name"
+        <input 
+        type='text' 
+        placeholder='Enter Mobile Name' 
         value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <br />
-      <input
-        type="text"
-        placeholder="Enter model Name"
+         onChange={(e)=>{
+            setName(e.target.value)
+        }} 
+        />
+        <br />
+        <input 
+        type='text' 
+        placeholder='Enter model Name' 
         value={modelName}
-        onChange={(e) => {
-          setModelName(e.target.value);
-        }}
-      />
-      <br />
+         onChange={(e)=>{
+            setModelName(e.target.value)
+        }} 
+        />
+        <br />
 
-      <input
-        type="text"
-        placeholder="Enter Image url"
+        <input 
+        type='text' 
+        placeholder='Enter Image url' 
         value={imgLink}
-        onChange={(e) => {
-          setImgLink(e.target.value);
-        }}
-      />
-      <br />
+         onChange={(e)=>{
+            setImgLink(e.target.value)
+        }} 
+        />
+        <br />
 
-      <button
-        className="btn"
-        onClick={() => {
-          updateArr("name");
-        }}
-      >
-        update data
-        <FontAwesomeIcon icon={faDollarSign} />
-      </button>
-      <div>
-        {itemList?.length > 0 ? (
-          itemList?.map((d, i) => {
-            return (
-              <>
-                <div key={i} onClick={() => navigateMe(d)}>
-                  <h1>{d?.name}</h1>
-                  <p>{d?.mobileModel}</p>
-                  <img src={d?.img} />
+                <button className='btn' onClick={()=>{
+                    updateArr('name')
+                }}>update data</button>
+                <div>
+                    {
+                       itemList?.length > 0 ? itemList?.map((d,i)=>{
+                            return(
+                                <>
+                                <div key={i} onClick={()=>navigateMe(d)}>
+                                <h1>{d?.name}</h1>
+                                <p>{d?.mobileModel}</p>
+                                <img src={d?.img}  height={300} width={300}/>
+                                </div>
+
+                                </>
+                            )
+                        })
+                        :(
+                            <>
+                            no data available
+                            </>
+                        )
+                    }
                 </div>
-              </>
-            );
-          })
-        ) : (
-          <>no data available</>
-        )}
-      </div>
-    </>
-  );
-};
+        </>
+    )
+}
 
-export default Button;
+export default Button
--- src/pages/UserPage/Addlist.jsx
+++ src/pages/UserPage/Addlist.jsx
@@ -1,10 +1,41 @@
+import { useEffect } from "react"
 import Button from "../../components/Button"
 
-const AddList =({})=>{
+const AddList = ({ }) => {
 
+    // useEffect(() => {
+    //     async function getApi(){
+    //         try{
+    //         let res =await fetch("http://localhost:5000/products/1")
+    //         let data =await res.json()
+    //         console.log("d--->",data)
+    //     }
+    //     catch(er){
+    //         console.log("err--->",er)
+    //     }
+    //     }
+    //     getApi()
+    // }, [])
 
+    async function postApi(){
+        try{
+        const jsonData = { key1: 'value1', key2: 'value2' };
+        let res =await fetch("https://jsonplaceholder.typicode.com/todos/1",{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(jsonData) 
+          })
+        let data =await res.json()
+        console.log("d--->",data)
+    }
+    catch(er){
+        console.log("err--->",er)
+    }
+    }
 
-    return(
+    return (
         <>
             <Button />
         </>
